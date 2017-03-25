7d439c8656a1b740d06234e32a4b4de27829512f
snowcheng <707818825@qq.com>
2017-03-26 01:22:46 +0800

    This is the layout

---
+++ Layout/README.md
@@ -0,0 +1,16 @@
+# Android布局实验
+Layout界面布局示例
+
+![初始化界面](https://github.com/lyb707818825/fjnuAndroid/blob/master/Layout/picture/2017-03-26-011555_329x620_scrot.png)
+***
+线性布局
+
+![线性布局](https://github.com/lyb707818825/fjnuAndroid/blob/master/Layout/picture/2017-03-26-011555_329x620_scrot.png)
+***
+相对布局
+
+![相对布局](https://github.com/lyb707818825/fjnuAndroid/blob/master/Layout/picture/2017-03-26-011642_329x620_scrot.png)
+***
+表格布局
+
+![表格布局](https://github.com/lyb707818825/fjnuAndroid/blob/master/Layout/picture/2017-03-26-011647_329x620_scrot.png)
---
+++ Layout/app/src/main/java/com/example/snowcheng/layout/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.snowcheng.layout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1=(Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LinearLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button2=(Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RelativeLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button3=(Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,TableLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
